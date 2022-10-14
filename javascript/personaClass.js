7a646691672c48c242b9523d8eaa99f0ba991f32
Franco Beber <fbeber73@gmail.com>
2022-10-14 20:47:14 -0300

    segunda entrega finalizo

---
+++ javascript/personaClass.js
@@ -0,0 +1,15 @@
+class Persona {
+  id;
+  nombre;
+  apellido;
+  edad;
+  dni;
+
+  constructor(id, nombre, apellido, edad, dni) {
+    this.id = id;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.dni = dni;
+  }
+}
